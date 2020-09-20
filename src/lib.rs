63e385b099b7560046d48a9faebd45973f514ab8
BBrown <bbrown@gorillagroup.com>
2020-09-20 10:42:55 -0500

    simplifying example

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,12 @@ use pulsar::{
 use core::marker;
 
 #[derive(Serialize, Deserialize)]
-pub struct PulsarMessageWrapper<'a, T> {
+pub struct PulsarMessageWrapper<T> {
     pub request_id: String,
-    pub session_id: String,
-    pub initiating_service: String,
-    pub node: String,
-    pub user_id: String,
-    pub headers: HashMap<String, String>,
     pub data: T,
-    _marker: marker::PhantomData<&'a T>,
 }
 
-impl<'a, T: Serialize> SerializeMessage for PulsarMessageWrapper<'a, T> {
+impl<T: Serialize> SerializeMessage for PulsarMessageWrapper<T> {
     fn serialize_message(input: Self) -> Result<producer::Message, PulsarError> {
         let payload = serde_json::to_vec(&input).map_err(|e| PulsarError::Custom(e.to_string()))?;
         Ok(producer::Message {
@@ -27,10 +21,10 @@ impl<'a, T: Serialize> SerializeMessage for PulsarMessageWrapper<'a, T> {
     }
 }
 
-impl<'de: 'a, 'a, T> DeserializeMessage for PulsarMessageWrapper<'a, T>
+impl<'de: 'a, 'a, T> DeserializeMessage for PulsarMessageWrapper<T>
     where
         T: Deserialize<'de> {
-    type Output = Result<PulsarMessageWrapper<'a, T>, serde_json::Error>;
+    type Output = Result<PulsarMessageWrapper<T>, serde_json::Error>;
 
     fn deserialize_message(payload: &Payload) -> Self::Output {
         serde_json::from_slice(&payload.data)
